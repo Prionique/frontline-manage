f52e450c22d9e9f6307cdc671bff12befe755772
Primon <115495464+Prionique@users.noreply.github.com>
2024-02-28 08:38:21 +0200

    Add files via upload

---
+++ script/info.js
@@ -0,0 +1,177 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  get,
+  remove
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { config } from "./switch.js";
+
+const app = initializeApp(config);
+const db = getDatabase(app);
+
+const kick_switch = document.getElementById("kickboxSwitch");
+const mma_switch = document.getElementById("mmaSwitch");
+
+const kick_cost_switch = document.getElementById("kickboxCostSwitch");
+const mma_cost_switch = document.getElementById("mmaCostSwitch");
+
+const date = document.getElementById("userDate")
+
+const nameElement = document.getElementById("username");
+let id = new URLSearchParams(window.location.search);
+
+get(ref(db, "users")).then((sn) => {
+    Object.keys(sn.val()).forEach((el) => {
+        get(ref(db, `/users/${el}`)).then((sn_) => {
+            if (id.get("id") === sn.val()[el]["id"].toString()){
+                if (sn_.val()["dir"]) {
+                    if (Array.from(sn_.val()["dir"]).includes("kick")) {
+                        kick_switch.checked=true
+                        kick_cost_switch.disabled = false
+                        if (sn_.val()["cost"]["kick"] === "free") {
+                            kick_cost_switch.checked = true
+                        }
+                    } 
+                    if (Array.from(sn_.val()["dir"]).includes("mma")) {
+                        mma_switch.checked=true
+                        mma_cost_switch.disabled = false
+                        if (sn_.val()["cost"]["mma"] === "free") {
+                            mma_cost_switch.checked = true
+                        }
+                    }
+                }
+                date.value = sn_.val()["date"]
+
+
+            }
+        })
+    })
+})
+kick_switch.addEventListener("click", function () {
+    get(ref(db, "users")).then((sn) => {
+        Object.keys(sn.val()).forEach((el) => {
+            get(ref(db, `/users/${el}`)).then((sn_) => {
+                if (id.get("id") === sn.val()[el]["id"].toString()) {
+                    let dt = []; // Initialize dt here
+                    if (sn_.val()["dir"]) { // Check if 'dir' exists
+                        dt = Object.values(sn_.val()["dir"]); // If it exists, assign its value to dt
+                    }
+                    if (kick_switch.checked) {
+                        kick_cost_switch.disabled = false
+                        if (!dt.includes("kick")) {
+                            dt.push("kick");
+                            set(ref(db, `users/${el}/dir`), dt);
+                        }
+                    } else {
+                        kick_cost_switch.checked = false
+                        kick_cost_switch.disabled = true
+
+
+                        dt = dt.filter((el) => el !== "kick");
+                        set(ref(db, `users/${el}/dir`), dt);
+                        set(ref(db, `users/${el}/cost/kick`), "paid")
+                    }
+                }
+            });
+        });
+    });
+});
+
+mma_switch.addEventListener("click", function () {
+    get(ref(db, "users")).then((sn) => {
+        Object.keys(sn.val()).forEach((el) => {
+            get(ref(db, `/users/${el}`)).then((sn_) => {
+                if (id.get("id") === sn.val()[el]["id"].toString()) {
+                    let dt = []; // Initialize dt here
+                    if (sn_.val()["dir"]) { // Check if 'dir' exists
+                        dt = Object.values(sn_.val()["dir"]); // If it exists, assign its value to dt
+                    }
+                    if (mma_switch.checked) {
+                        mma_cost_switch.disabled = false
+                        if (!dt.includes("mma")) {
+                            dt.push("mma");
+                            set(ref(db, `users/${el}/dir`), dt);
+                        }
+                    } else {
+                        mma_cost_switch.disabled = true
+                        mma_cost_switch.checked = false
+                        dt = dt.filter((el) => el !== "mma");
+                        set(ref(db, `users/${el}/dir`), dt);
+                        set(ref(db, `users/${el}/cost/mma`), "paid")
+                    }
+                }
+            });
+        });
+    });
+});
+kick_cost_switch.addEventListener("click", function () {
+    get(ref(db, "users")).then((sn) => {
+        Object.keys(sn.val()).forEach((el) => {
+            get(ref(db, `/users/${el}`)).then((sn_) => {
+                if (id.get("id") === sn.val()[el]["id"].toString()) {
+                    if (kick_cost_switch.checked) {
+                        set(ref(db, `/users/${el}/cost/kick`), "free")
+                    } else {
+                        set(ref(db, `/users/${el}/cost/kick`), "paid")
+                    }
+                }
+            });
+        });
+    });
+})
+mma_cost_switch.addEventListener("click", function () {
+    get(ref(db, "users")).then((sn) => {
+        Object.keys(sn.val()).forEach((el) => {
+            get(ref(db, `/users/${el}`)).then((sn_) => {
+                if (id.get("id") === sn.val()[el]["id"].toString()) {
+                    if (mma_cost_switch.checked) {
+                        set(ref(db, `/users/${el}/cost/mma`), "free")
+                    } else {
+                        set(ref(db, `/users/${el}/cost/mma`), "paid")
+                    }
+                }
+            });
+        });
+    });
+})
+
+date.addEventListener("change", function () {
+   
+
+    get(ref(db, "users")).then((sn) => {
+        Object.keys(sn.val()).forEach((el) => {
+            get(ref(db, `/users/${el}`)).then((sn_) => {
+                if (id.get("id") === sn.val()[el]["id"].toString()) {
+                    set(ref(db, `users/${el}/date`), date.value.trim())
+                }
+            });
+        });
+    });
+    
+})
+get(ref(db, "users")).then((snapshot) => {
+  let data = snapshot.val();
+  Object.keys(data).forEach((el) => {
+    if (data[el]["id"].toString() === id.get("id")) {
+      nameElement.value = el;
+      nameElement.setAttribute("readonly", "readonly");
+    }
+  });
+});
+
+document.getElementById("delete").addEventListener("click", function() {
+    get(ref(db, "users")).then((sn) => {
+        Object.keys(sn.val()).forEach((el) => {
+            get(ref(db, `/users/${el}`)).then((sn_) => {
+                if (id.get("id") === sn.val()[el]["id"].toString()) {
+                    if (confirm("Точно удалить?")) {
+                        remove(ref(db, `/users/${el}`))
+                        window.location.href = 'members.html'
+                    }
+                }
+            });
+        });
+    });
+})
---
+++ script/switch.js
@@ -0,0 +1,67 @@
+const _0x2e9528 = _0x319a;
+function _0x319a(_0xab42d, _0xf63696) {
+  const _0x376af8 = _0x376a();
+  return (
+    (_0x319a = function (_0x319ada, _0x432f11) {
+      _0x319ada = _0x319ada - 0x1c3;
+      let _0x2ccf17 = _0x376af8[_0x319ada];
+      return _0x2ccf17;
+    }),
+    _0x319a(_0xab42d, _0xf63696)
+  );
+}
+function _0x376a() {
+  const _0x6ab7a7 = [
+    "20665IMCwHd",
+    "800874BlWNoo",
+    "1293197JJGylJ",
+    "64ZmSoQq",
+    "43463DXZajo",
+    "2584894wPBGom",
+    "frontline-sport.firebaseapp.com",
+    "960NeRWpw",
+    "G-4S6SFCJ90N",
+    "2910951cafOEa",
+    "446981204542",
+    "frontline-sport.appspot.com",
+    "1:446981204542:web:23010639c79d8a6cef7f6f",
+    "AIzaSyAkknZyH3N6-ZMm31c8o3zFQSv8s45YMDY",
+    "22677228QEYFOj",
+  ];
+  _0x376a = function () {
+    return _0x6ab7a7;
+  };
+  return _0x376a();
+}
+(function (_0x2454ff, _0x533561) {
+  const _0x19930f = _0x319a,
+    _0x5f0381 = _0x2454ff();
+  while (!![]) {
+    try {
+      const _0x2d166d =
+        parseInt(_0x19930f(0x1ca)) / 0x1 +
+        -parseInt(_0x19930f(0x1cd)) / 0x2 +
+        -parseInt(_0x19930f(0x1d1)) / 0x3 +
+        (parseInt(_0x19930f(0x1cf)) / 0x4) *
+          (-parseInt(_0x19930f(0x1c8)) / 0x5) +
+        parseInt(_0x19930f(0x1c9)) / 0x6 +
+        (-parseInt(_0x19930f(0x1cc)) / 0x7) *
+          (-parseInt(_0x19930f(0x1cb)) / 0x8) +
+        parseInt(_0x19930f(0x1c7)) / 0x9;
+      if (_0x2d166d === _0x533561) break;
+      else _0x5f0381["push"](_0x5f0381["shift"]());
+    } catch (_0x105c5b) {
+      _0x5f0381["push"](_0x5f0381["shift"]());
+    }
+  }
+})(_0x376a, 0xb4fec);
+export const config = {
+  apiKey: _0x2e9528(0x1c6),
+  authDomain: _0x2e9528(0x1ce),
+  databaseURL: "https://frontline-sport-default-rtdb.firebaseio.com",
+  projectId: "frontline-sport",
+  storageBucket: _0x2e9528(0x1c4),
+  messagingSenderId: _0x2e9528(0x1c3),
+  appId: _0x2e9528(0x1c5),
+  measurementId: _0x2e9528(0x1d0),
+};
